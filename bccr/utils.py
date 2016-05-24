909d116dbd832e77bc6554c00138cc70dc7adfb4
randall-romero <romero-aguilar.1@osu.edu>
2016-05-24 17:28:03 -0600

    functions removeEmptyColumns and isColumnEmpty no longer needed
    new function is_leap_year

--- bccr/utils.py
+++ bccr/utils.py
@@ -2,15 +2,6 @@ import numpy as np
 import pandas as pd
 import re
 
-def isColumnEmpty(x):
-    return np.all(np.isnan(x))
-
-
-def removeEmptyColumns(data):
-    emptyColumns = data.apply(isColumnEmpty).values
-    return data.iloc[:, ~emptyColumns] if np.any(emptyColumns) else data
-
-
 
 def findFirstElement(pattern, stringList):
     idx = 0
@@ -40,10 +31,11 @@ def tidy(data, timeindex, freq, func, colnames):
     if isinstance(data, pd.Series):
         data = data.apply(fixCommas)
         data.name = colnames
+        data.dropna(inplace=True)
     elif isinstance(data, pd.DataFrame):
         data.columns = colnames
-        data = data.applymap(fixCommas)
-        data = removeEmptyColumns(data)
+        data = data.applymap(fixCommas).dropna(0, 'all').dropna(1, 'all')
+        #data = removeEmptyColumns(data)
 
     else:
         raise NotImplemented
@@ -58,3 +50,10 @@ def seriesAsDict(series):
     alreadyDict = isinstance(series, dict)
     return series if alreadyDict else {chartNumber: '' for chartNumber in series}
 
+
+def is_leap_year(years):
+    """Determine whether a year is a leap year."""
+    return np.bitwise_and(years % 4 == 0,
+                          np.bitwise_or(years % 100 != 0,
+                                        years % 400 == 0)
+                          )
